5b54edbba725bea2965a9a39cb5d48fec1d3078c
TakaoTakagi <deosanches@hotmail.com>
2025-06-26 19:07:24 -0300

    Corrigido package.json e adicionado firebase.js

--- fichas-rpg/src/firebase.js
+++ fichas-rpg/src/firebase.js
@@ -1,16 +1,17 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2xyCO24rgr76x0CYUUIQIjgv0aUHbEbQ",
-  authDomain: "perditteficha.firebaseapp.com",
-  projectId: "perditteficha",
-  storageBucket: "perditteficha.firebasestorage.app",
-  messagingSenderId: "681234689710",
-  appId: "1:681234689710:web:1654b2b21740267a2a873f"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "SEU_PROJETO_ID",
+  storageBucket: "SEU_BUCKET.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
